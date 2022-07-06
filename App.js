5122657e71a6f7eddadf5f5c24bed3a345073925
cor2ba <cordobagabrielignacio@gmail.com>
2022-07-06 12:38:59 -0300

    Estructura 1

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+const Stack = createStackNavigator();
+import LogIn from "./components/logIn";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Log In" component={LogIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/logIn.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Button, TextInput, View, Image, StyleSheet, Text } from "react-native";
+
+const LogIn = () => {
+  return (
+    <View style={styles.countainer}>
+      <Image
+        style={styles.image}
+        source={{
+          uri: "https://avatars.githubusercontent.com/u/57154655?s=200&v=4",
+        }}
+      ></Image>
+      <View style={styles.inputs}>
+        <Text style={styles.text}>Email</Text>
+        <TextInput
+          placeholderTextColor={"grey"}
+          placeholder="soyhenry@gmail.com"
+        ></TextInput>
+        <Text style={styles.text}>Contraseña</Text>
+        <TextInput
+          placeholderTextColor={"grey"}
+          placeholder="*******"
+        ></TextInput>
+      </View>
+      <View style={styles.btn}>
+        <Button title="Login"></Button>
+        <Button title="Registrarse"></Button>
+      </View>
+      <Text style={styles.text}>¿Olvidaste la contraseña?</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  countainer: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    backgroundColor: "#140152",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  inputs: {},
+  btn: {
+    flexDirection: "column",
+  },
+  text: {
+    color: "#fff",
+  },
+  passwordHelp: {
+    color: "#fff",
+  },
+});
+
+export default LogIn;
