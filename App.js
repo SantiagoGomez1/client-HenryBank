a58e301dac3886ebc352694094cc426c156c3ff4
cor2ba <cordobagabrielignacio@gmail.com>
2022-07-06 14:28:53 -0300

    Register B

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 const Stack = createStackNavigator();
-import LogIn from "./components/logIn";
+import LogIn from "./components/LogIn/logIn";
+import RegisterB from "./components/RegisterB/registerB";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Log In">
         <Stack.Screen name="Log In" component={LogIn} />
+        <Stack.Screen name="Register B" component={RegisterB} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/LogIn/logIn.jsx
+++ components/LogIn/logIn.jsx
@@ -50,6 +50,7 @@ const styles = StyleSheet.create({
     flexDirection: "column",
   },
   text: {
+    fontSize: 25,
     color: "#fff",
   },
   passwordHelp: {
