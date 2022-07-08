9669f2cd219db6b11943790cbbd70c253eca45a3
cor2ba <cordobagabrielignacio@gmail.com>
2022-07-08 15:38:01 -0300

    Log in validado

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,16 @@
-// import axios from "axios";
+import axios from "axios";
 import { data } from "../../../response";
 
 export const GET_USERS = "GET_USERS";
 export const GET_USER = "GET_USER";
+export const LOG_IN = "LOG_IN";
+
+export const logIn = (form) => async (dispatch) => {
+  console.log(form);
+  const response = await axios.post(`http://localhost:3001/login`, form);
+  const payload = await response.data;
+  return dispatch({ type: LOG_IN, payload });
+};
 
 export const getUsers = () => {
   return {
