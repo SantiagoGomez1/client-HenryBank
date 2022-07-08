54a65f3adf461168cd299613ce433a126c555931
Yeison Villegas <yeisonvillegas07@gmail.com>
2022-07-08 11:23:16 -0500

    Incorporada una pantalla en el app y agregados dos componentes mas

---
+++ src/components/CardCoinsInvestorBuy/CardCoinsInvestorBuy.jsx
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import React from "react";
+
+export default function CardCoinsInvestorBuy({ id, image, name, symbol }) {
+  console.log(image);
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => console.log(id)}>
+      <View style={styles.caract}>
+        <Image style={styles.img} source={{ uri: image }} />
+        <Text style={{ color: "white" }}>{name}</Text>
+        <Text style={{ color: "white" }}>{symbol}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgba(25, 23, 61, 0.5)",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "rgba(255, 255, 255, 0.5)",
+    margin: 10,
+    borderRadius: 8,
+    padding: 10,
+  },
+  img: {
+    width: 20,
+    height: 20,
+  },
+  caract: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+});
