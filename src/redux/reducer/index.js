29e6fbf857a9c7b7d673eff442e22208f5e68513
Yeison Villegas <yeisonvillegas07@gmail.com>
2022-07-09 12:18:08 -0500

    Buscador del apartado investor/buy listo

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,8 +1,12 @@
-
-import { GET_USERS, GET_USER, LOG_IN, POST_USER_DATA} from "../actions/index";
+import {
+  GET_USERS,
+  GET_USER,
+  LOG_IN,
+  POST_USER_DATA,
+  SEARCH_COINS,
+} from "../actions/index";
 
 import { GET_COINS } from "../actions/index";
-
 
 const initialState = {
   users: [],
@@ -32,10 +36,19 @@ const rootReducer = (state = initialState, action) => {
     case POST_USER_DATA:
       return {
         ...state,
+      };
     case GET_COINS:
       return {
         ...state,
         coins: action.payload,
+      };
+    case SEARCH_COINS:
+      const coinSearch = state.coins.filter((element) =>
+        element.name.toLowerCase().includes(action.payload.toLowerCase())
+      );
+      return {
+        ...state,
+        coins: coinSearch,
       };
     default:
       return state;
