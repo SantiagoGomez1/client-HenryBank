6b061f8df8d1ed491c03562024fd4be40fd2f88f
Emanuel Juri <emanueljuri@gmail.com>
2022-07-16 14:33:39 -0300

    Historial de movimientos OK

--- src/components/UserCardMovimientos/UserCardMovimientos.jsx
+++ src/components/UserCardMovimientos/UserCardMovimientos.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 import { View, Text, Image, StyleSheet } from "react-native";
+import { Icon } from "@rneui/themed";
+
 
 const CardUserMovimientos = ({ id, image, name, amount, date }) => {
   return (
     <View style={styles.container}>
       <View style={styles.subContainer}>
         <View style={styles.borde}>
-          <Image style={styles.img} source={{ uri: `${image}` }} />
+          {/* <Image style={styles.img} source={{ uri: `${image}` }} /> */}
+          <Icon
+          name={image}
+          type='material-community'          
+          size={35}
+          color={amount<0 ? 'red' : '#00FF00'} />
         </View>
         <View style={styles.data}>
           <Text style={styles.text}>{name}</Text>
@@ -15,7 +22,7 @@ const CardUserMovimientos = ({ id, image, name, amount, date }) => {
         </View>
       </View>
       <View>
-        <Text style={styles.textAmount}>{amount}</Text>
+        <Text style={styles.textAmount}>$ {amount}</Text>
       </View>
     </View>
   );
@@ -24,31 +31,35 @@ const CardUserMovimientos = ({ id, image, name, amount, date }) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    padding: 10,
+    padding: 10,    
+    justifyContent: 'space-between',  
   },
   subContainer: {
     flexDirection: "row",
-    paddingRight:25
+    paddingRight:25,
+    alignItems: 'center',
   },
   img: {
-    width: 40,
-    height: 40,
+    width: 38,
+    height: 38,
   },
   borde: {
     alignItems: "center",
-    justifyContent: "center",
-    borderWidth: 1,
+    justifyContent: "center",    
+    // borderWidth: 1,
     borderColor: "white",
     borderRadius: 50,
-    backgroundColor: "white",
-    width: 70,
-    height: 70,
+    // backgroundColor: "white",
+    width: 40,
+    height: 40,    
   },
   text: {
     color: "white",
     fontWeight: "bold",
   },
   textAmount: {
+    alignItems: 'center',
+    paddingLeft: 10,
     color: "white",
     paddingTop:15,
     backgroundColor: "rgba(25, 23, 61, 0.5)",
