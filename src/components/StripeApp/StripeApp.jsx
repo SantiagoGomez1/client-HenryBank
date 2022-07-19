28b3303993981f9c4243d1e773e3627eb860e5a7
cor2ba <cordobagabrielignacio@gmail.com>
2022-07-19 18:01:34 -0300

    Validations  Stripe App

--- src/components/StripeApp/StripeApp.jsx
+++ src/components/StripeApp/StripeApp.jsx
@@ -8,19 +8,28 @@ import { useNavigation } from "@react-navigation/native";
 const API_URL = "https://h-bank.herokuapp.com/user/recharge";
 
 const StripeApp = () => {
-  const dispatch = useDispatch();
   const navigation = useNavigation();
-
-  const setScreen = (screen) => {
-    dispatch(renderScreen(screen));
-  };
   const token = useSelector((state) => state.logIn.token);
 
   const [input, setInput] = useState({
     amount: 0,
   });
 
-  const [errors, setError] = useState("");
+  const [errorMoney, setErrorMoney] = useState(0);
+  const validateData = () => {
+    let isValid = true;
+
+    if (input.amount <= 100) {
+      setErrorMoney("Deberia ingresar al menos $100.00");
+      isValid = false;
+    }
+    if (input.amount === 0) {
+      setErrorMoney("Deberia ingresar un monto");
+      isValid = false;
+    }
+
+    return isValid;
+  };
 
   const { confirmPayment, loading } = useConfirmPayment();
 
@@ -34,60 +43,41 @@ const StripeApp = () => {
   };
 
   async function handlePayPress() {
-    const response = await axios.post(
-      API_URL,
-      {
-        amount: input,
-        paymentMethodType: "card",
-        currency: "ars",
-      },
-      config
-    );
-    const { clientSecret } = await response.data;
-    const { error, paymentIntent } = await confirmPayment(clientSecret, {
-      type: "Card",
-      billingDetails: { estado: "ok" },
-    });
+    if (!validateData()) {
+      return console.log("hola");
+    } else {
+      const response = await axios.post(
+        API_URL,
+        {
+          amount: input,
+          paymentMethodType: "card",
+          currency: "ars",
+        },
+        config
+      );
+      const { clientSecret } = await response.data;
+      const { error, paymentIntent } = await confirmPayment(clientSecret, {
+        type: "Card",
+        billingDetails: { estado: "ok" },
+      });
 
-    if (error) {
-      Alert.alert(`Error code: ${error.code}`, error.message);
-    } else if (paymentIntent) {
-      /* Alert.alert(`Success`, `Payment successful: ${paymentIntent.id}`); */
-      navigation.navigate("SuccessOperacion");
+      if (error) {
+        Alert.alert(`Error code: ${error.code}`, error.message);
+      } else if (paymentIntent) {
+        navigation.navigate("SuccessOperacion");
+      }
     }
   }
-
-  // const validateData = () => {
-  //   setError("");
-  //   let isValid = true;
-  //   if (input.amount.length === 0) {
-  //     setError("");
-  //     isValid = false;
-  //   }
-  //   if (input.amount === 0) {
-  //     setError("");
-  //     isValid = false;
-  //   }
-  //   if (input.amount > 100000) {
-  //     setError("El monto maximo son $100.000");
-  //     isValid = false;
-  //   }
-  //   return isValid;
-  // };
-
   return (
     <View style={styles.container}>
-      <View style={styles.containerAmount}>
-        <TextInput
-          style={styles.input}
-          placeholder="$00,00"
-          placeholderTextColor="white"
-          onChange={(e) => handleOnChange(e)}
-          keyboardType="number-pad"
-          errorMessage={errors}
-        />
-      </View>
-
+      <TextInput
+        style={styles.input}
+        placeholder="$100,00"
+        placeholderTextColor="white"
+        onChange={(e) => handleOnChange(e)}
+        keyboardType="number-pad"
+        errorMessage={errorMoney}
+      />
       <CardField
         postalCodeEnabled={false}
         placeholder={{
@@ -96,25 +86,22 @@ const StripeApp = () => {
         cardStyle={styles.card}
         style={styles.cardContainer}
       />
-      <View style={{ paddingBottom: 40 }}>
-        {errors ? <Text style={{ color: "red" }}>{errors}</Text> : null}
-        <Button onPress={handlePayPress} title="Confirmar" disable={loading} />
-      </View>
+      {errorMoney ? <Text style={{ color: "red" }}>{errorMoney}</Text> : null}
+      <Button onPress={handlePayPress} title="Confirmar" disable={loading} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  containerAmount: {
-    paddingVertical: 45,
+  container: {
+    flex: 1,
+    justifyContent: "space-around",
   },
   input: {
     textAlign: "center",
     color: "white",
     fontSize: 40,
     fontWeight: "bold",
-    width: 300,
-    height: 80,
   },
   card: {
     backgroundColor: "#efefefef",
@@ -122,7 +109,6 @@ const styles = StyleSheet.create({
   cardContainer: {
     height: 50,
     width: 300,
-    marginVertical: 30,
     borderRadius: 8,
   },
 });
