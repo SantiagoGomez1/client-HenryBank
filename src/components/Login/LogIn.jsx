754bf43fa8690f8aa663a85817db0c5d03fa20f1
Maximiliano Rocha <maxi.jrocha97@gmail.com>
2022-07-24 13:23:54 -0300

    add Login Google

--- src/components/Login/LogIn.jsx
+++ src/components/Login/LogIn.jsx
@@ -1,21 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
-  Button,
   View,
   Image,
   StyleSheet,
   Text,
   Dimensions,
-} from "react-native";
-import { Input, Icon } from "react-native-elements";
-import { LinearGradient } from "expo-linear-gradient";
-import Constants from "expo-constants";
-import { logIn, cleanLogIn } from "../../redux/actions";
-import { useDispatch } from "react-redux";
-import { useNavigation } from "@react-navigation/native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+  Button,
+} from 'react-native';
+import { Input, Icon, Button as Button2 } from 'react-native-elements';
+import { LinearGradient } from 'expo-linear-gradient';
+import Constants from 'expo-constants';
+import { logIn, cleanLogIn } from '../../redux/actions';
+import { useDispatch } from 'react-redux';
+import { TabRouter, useNavigation } from '@react-navigation/native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
 
-var { height } = Dimensions.get("window");
+WebBrowser.maybeCompleteAuthSession();
+
+var { height } = Dimensions.get('window');
 
 const LogIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -23,7 +27,7 @@ const LogIn = () => {
   const navigation = useNavigation();
 
   const goForgotPassword = () => {
-    navigation.navigate("ForgotPasswordA");
+    navigation.navigate('ForgotPasswordA');
   };
 
   useEffect(() => {
@@ -31,12 +35,12 @@ const LogIn = () => {
   });
 
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
-  const [errorEmail, setErrorEmail] = useState("");
-  const [errorPassword, setErrorPassword] = useState("");
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
 
   const handleOnChange = (e, type) => {
     setFormData({ ...formData, [type]: e.nativeEvent.text });
@@ -45,56 +49,86 @@ const LogIn = () => {
   const registerUser = () => {
     if (!validateData()) return;
     dispatch(logIn(formData));
-    navigation.navigate("Confirmation");
+    navigation.navigate('Confirmation');
   };
 
   const validateData = () => {
-    setErrorEmail("");
-    setErrorPassword("");
+    setErrorEmail('');
+    setErrorPassword('');
     let isValid = true;
 
     if (!validateEmail(formData.email)) {
-      setErrorEmail("Debes ingresar un E-mail válido");
+      setErrorEmail('Debes ingresar un E-mail válido');
       isValid = false;
     }
 
     if (formData.password.length < 8) {
-      setErrorPassword("Contraseña incorrecta");
+      setErrorPassword('Contraseña incorrecta');
       isValid = false;
     }
 
     return isValid;
   };
 
+  // GOOGLE
+  async function loadGoogleInfoUser(token) {
+    let response = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const userInfo = await response.json();
+
+    if (userInfo.email) {
+      navigation.navigate('RegisterB', {
+        user: { email: userInfo.email, image: userInfo.picture },
+      });
+    }
+  }
+
+  async function signInGoogle() {
+    const CLIENT_ID =
+      '3723003346-i8mc0vbgp92ik62gae0jieh350vmqegb.apps.googleusercontent.com';
+    const REDIRECT_URL = 'https://auth.expo.io/@maxiwee/HenryBank';
+    const RESPONSE_TYPE = 'token';
+    const SCOPE = encodeURI('profile email');
+
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+    const { params } = await AuthSession.startAsync({ authUrl });
+
+    loadGoogleInfoUser(params.access_token);
+
+    console.log('### PARAMS ', params);
+  }
+
   return (
     <KeyboardAwareScrollView style={styles.container}>
-      <LinearGradient colors={["#126492", "#140152"]} style={styles.background}>
+      <LinearGradient colors={['#126492', '#140152']} style={styles.background}>
         <Image
           style={styles.image}
-          source={require("../../imgs/HenryBank.png")}
+          source={require('../../imgs/HenryBank.png')}
         ></Image>
 
         <Input
           containerStyle={styles.input}
-          placeholder="Soyhenry@gmail.com"
-          label="E-mail"
-          onChange={(e) => handleOnChange(e, "email")}
+          placeholder='Soyhenry@gmail.com'
+          label='E-mail'
+          onChange={e => handleOnChange(e, 'email')}
           errorMessage={errorEmail}
           defaultValue={formData.email}
         ></Input>
         <Input
           containerStyle={styles.input}
-          placeholder="Soyhenry123"
-          label="Contraseña"
+          placeholder='Soyhenry123'
+          label='Contraseña'
           password={true}
           secureTextEntry={!showPassword}
-          onChange={(e) => handleOnChange(e, "password")}
+          onChange={e => handleOnChange(e, 'password')}
           errorMessage={errorPassword}
           defaultValue={formData.password}
           rightIcon={
             <Icon
-              type="material-community"
-              name={showPassword ? "eye-off-outline" : "eye-outline"}
+              type='material-community'
+              name={showPassword ? 'eye-off-outline' : 'eye-outline'}
               iconStyle={styles.icon}
               onPress={() => setShowPassword(!showPassword)}
             />
@@ -103,14 +137,15 @@ const LogIn = () => {
 
         <View style={styles.btn}>
           <Button
-            title="Log In"
-            color={"purple"}
+            title='Log In'
+            color={'purple'}
             onPress={() => registerUser()}
           ></Button>
+          <Button2 title='GOOGLE' onPress={() => signInGoogle()}></Button2>
           <Button
-            title="Registrarse"
-            color="trasparent"
-            onPress={() => navigation.navigate("RegisterA")}
+            title='Registrarse'
+            color='trasparent'
+            onPress={() => navigation.navigate('RegisterA')}
           ></Button>
         </View>
         <Text style={styles.text} onPress={() => goForgotPassword()}>
@@ -124,14 +159,14 @@ const LogIn = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#140152",
+    backgroundColor: '#140152',
   },
   background: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "space-around",
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'space-around',
     paddingTop: Constants.statusBarHeight,
     height: height,
   },
@@ -141,23 +176,23 @@ const styles = StyleSheet.create({
     borderRadius: 100,
   },
   input: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 25,
     paddingTop: 10,
-    width: "80%",
+    width: '80%',
   },
   icon: {
-    color: "#85929E",
+    color: '#85929E',
   },
   btn: {
-    flexDirection: "column",
+    flexDirection: 'column',
   },
   text: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 10,
   },
   passwordHelp: {
-    color: "#fff",
+    color: '#fff',
   },
 });
 
--- src/components/RegisterB/RegisterB.jsx
+++ src/components/RegisterB/RegisterB.jsx
@@ -1,39 +1,48 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { LinearGradient } from "expo-linear-gradient";
-import { Input, Icon } from "react-native-elements";
-import { useDispatch, useSelector } from "react-redux";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, ScrollView } from 'react-native';
+import {
+  useFocusEffect,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Input, Icon } from 'react-native-elements';
+import { useDispatch, useSelector } from 'react-redux';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
-import { postUserData, postUserDataCard } from "../../redux/actions";
+import { postUserData, postUserDataCard } from '../../redux/actions';
 import {
   DropdownComponentGender,
   DropdownComponentNationality,
   DropdownComponentCity,
-} from "./Dropdown";
+} from './Dropdown';
 
-import Constants from "expo-constants";
-import ImagePicker from "../ImagePicker/ImagePicker.jsx";
+import Constants from 'expo-constants';
+import ImagePicker from '../ImagePicker/ImagePicker.jsx';
 
 const RegisterB = () => {
   const dispatch = useDispatch();
-  const userMP = useSelector((state) => state.userMP);
+  const userMP = useSelector(state => state.userMP);
   const navigation = useNavigation();
   const goRegisterC = () => {
-    navigation.navigate("RegisterC");
+    navigation.navigate('RegisterC');
   };
 
+  // GOOGLE INFO USER
+
+  const route = useRoute();
+  const { user } = route.params;
+
   const [formData, setFormData] = useState({
-    name: "",
-    image:"",
-    lastName: "",
-    identity: "",
-    gender: "",
-    dateOfBirth: "",
-    city: "",
-    nationality: "",
-    address: "",
+    name: '',
+    image: '',
+    lastName: '',
+    identity: '',
+    gender: '',
+    dateOfBirth: '',
+    city: '',
+    nationality: '',
+    address: '',
   });
   //----------------------States Picker-Date----------------------//
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
@@ -44,16 +53,16 @@ const RegisterB = () => {
     setDatePickerVisibility(false);
   };
   //----------------------States Errors----------------------//
-  const [errorName, setErrorName] = useState("");
-  const [errorIdentity, setErrorIdentity] = useState("");
-  const [errorDateOfBirth, setErrorDateOfBirth] = useState("");
+  const [errorName, setErrorName] = useState('');
+  const [errorIdentity, setErrorIdentity] = useState('');
+  const [errorDateOfBirth, setErrorDateOfBirth] = useState('');
   //---------------------------------------------------------//
 
   const handleOnChange = (e, type) => {
     setFormData({ ...formData, [type]: e.nativeEvent.text });
   };
 
-  const handleConfirm = (date) => {
+  const handleConfirm = date => {
     const year = JSON.stringify(date).substring(1, 5);
     const months = JSON.stringify(date).substring(6, 8);
     const days = JSON.stringify(date).substring(9, 11);
@@ -63,15 +72,15 @@ const RegisterB = () => {
     hideDatePicker();
   };
 
-  const handleGender = (props) => {
+  const handleGender = props => {
     setFormData({ ...formData, gender: props });
   };
 
-  const handleNationality = (props) => {
+  const handleNationality = props => {
     setFormData({ ...formData, nationality: props });
   };
 
-  const handleCity = (props) => {
+  const handleCity = props => {
     setFormData({ ...formData, city: props });
   };
 
@@ -87,28 +96,28 @@ const RegisterB = () => {
 
   //----------------------Funtion Validation----------------------//
   const validateData = () => {
-    setErrorName("");
-    setErrorIdentity("");
+    setErrorName('');
+    setErrorIdentity('');
     let isValid = true;
 
     if (!validateName(formData.name)) {
-      setErrorName("Debes ingresar solo letras");
+      setErrorName('Debes ingresar solo letras');
       isValid = false;
     }
     if (!validateName(formData.lastName)) {
-      setErrorName("Debes ingresar solo letras");
+      setErrorName('Debes ingresar solo letras');
       isValid = false;
     }
     if (formData.identity.length < 8) {
-      setErrorIdentity("Debes ingresar un número válido");
+      setErrorIdentity('Debes ingresar un número válido');
       isValid = false;
     }
     if (!validateIdentity(formData.identity)) {
-      setErrorIdentity("Debes ingresar solo números");
+      setErrorIdentity('Debes ingresar solo números');
       isValid = false;
     }
     if (!validateDateOfBirth(formData.dateOfBirth)) {
-      setErrorDateOfBirth("Debes ingresar un formato correcto");
+      setErrorDateOfBirth('Debes ingresar un formato correcto');
       isValid = false;
     }
 
@@ -128,37 +137,37 @@ const RegisterB = () => {
       /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
     return re.test(dateOfBirth);
   };
-  console.log("llego la img",formData.image)
+
   return (
     <ScrollView style={styles.container}>
-      <LinearGradient colors={["#126492", "#140152"]} style={styles.background}>
+      <LinearGradient colors={['#126492', '#140152']} style={styles.background}>
         <Text style={styles.textMain}>Regístrate</Text>
 
-        <ImagePicker setFormData={setFormData} formData={formData}/>
+        <ImagePicker setFormData={setFormData} formData={formData} />
 
         <Input
           containerStyle={styles.input}
-          placeholder="Ingresa tu nombre"
-          label="Nombre"
-          onChange={(e) => handleOnChange(e, "name")}
+          placeholder='Ingresa tu nombre'
+          label='Nombre'
+          onChange={e => handleOnChange(e, 'name')}
           errorMessage={errorName}
           defaultValue={formData.name}
         />
 
         <Input
           containerStyle={styles.input}
-          placeholder="Ingresa tu apellido"
-          label="Apellido"
-          onChange={(e) => handleOnChange(e, "lastName")}
+          placeholder='Ingresa tu apellido'
+          label='Apellido'
+          onChange={e => handleOnChange(e, 'lastName')}
           errorMessage={errorName}
           defaultValue={formData.lastName}
         />
 
         <Input
           containerStyle={styles.input}
-          placeholder="Solo números"
-          label="Cedula de Identidad / D.N.I"
-          onChange={(e) => handleOnChange(e, "identity")}
+          placeholder='Solo números'
+          label='Cedula de Identidad / D.N.I'
+          onChange={e => handleOnChange(e, 'identity')}
           errorMessage={errorIdentity}
           defaultValue={formData.identity}
         />
@@ -176,16 +185,16 @@ const RegisterB = () => {
 
         <Input
           containerStyle={styles.input}
-          placeholder="22/04/1995"
-          label="Fecha de Nacimiento"
-          onChange={(e) => handleOnChange(e, "dateOfBirth")}
+          placeholder='22/04/1995'
+          label='Fecha de Nacimiento'
+          onChange={e => handleOnChange(e, 'dateOfBirth')}
           errorMessage={errorDateOfBirth}
           handleConfirm={handleConfirm}
           defaultValue={handleConfirm ? handleConfirm : formData.dateOfBirth}
           rightIcon={
             <Icon
-              type="material-community"
-              name="calendar-account-outline"
+              type='material-community'
+              name='calendar-account-outline'
               iconStyle={styles.icon}
               onPress={() => showDatePicker()}
             />
@@ -193,7 +202,7 @@ const RegisterB = () => {
         />
         <DateTimePickerModal
           isVisible={isDatePickerVisible}
-          mode="date"
+          mode='date'
           onConfirm={handleConfirm}
           onCancel={hideDatePicker}
         />
@@ -222,16 +231,16 @@ const RegisterB = () => {
 
         <Input
           containerStyle={styles.input}
-          placeholder="Cerro Arco 4065"
-          label="Domicilio"
-          onChange={(e) => handleOnChange(e, "address")}
+          placeholder='Cerro Arco 4065'
+          label='Domicilio'
+          onChange={e => handleOnChange(e, 'address')}
           // errorMessage={errorEmail}
           defaultValue={formData.address}
         />
         <View style={styles.btn}>
           <Button
-            title="Siguiente"
-            color={"purple"}
+            title='Siguiente'
+            color={'purple'}
             onPress={() => registerUserPerData()}
           ></Button>
         </View>
@@ -246,28 +255,28 @@ const styles = StyleSheet.create({
   },
   background: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "space-around",
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'space-around',
     paddingTop: Constants.statusBarHeight,
   },
   textMain: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 45,
   },
   text: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 25,
   },
 
   input: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 25,
     margin: 15,
     paddingLeft: 25,
     paddingTop: 15,
-    width: "85%",
+    width: '85%',
   },
   btn: {
     margin: 20,
